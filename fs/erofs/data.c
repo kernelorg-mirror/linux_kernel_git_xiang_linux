994dae534d555de0848fdd8717d8d5c688405026
Gao Xiang <gaoxiang25@huawei.com>
2019-08-02 17:30:23 +0800

    erofs: add compression indexes support
    This patch adds EROFS compression indexes support,
    including legacy and compacted 2/4B indexes.
    
    In addition, it introduces an iterable L2P mapping
    operation called 'z_erofs_map_blocks_iter'.
    
    Compared with 'erofs_map_blocks', it avoids a number
    of redundant 'release and regrab' processes if they
    request the same meta page.
    
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>

--- fs/erofs/data.c
+++ fs/erofs/data.c
@@ -170,9 +170,15 @@ err_out:
 int erofs_map_blocks(struct inode *inode,
 		     struct erofs_map_blocks *map, int flags)
 {
-	if (is_inode_layout_compression(inode))
-		return -ENOTSUPP;
+	if (unlikely(is_inode_layout_compression(inode))) {
+		int err = z_erofs_map_blocks_iter(inode, map, flags);
 
+		if (map->mpage) {
+			put_page(map->mpage);
+			map->mpage = NULL;
+		}
+		return err;
+	}
 	return erofs_map_blocks_flatmode(inode, map, flags);
 }
 
